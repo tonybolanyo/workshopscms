0566fe3d6bba0c9558948a152f8e1493aa7978a6
Tony G. Bolaño <tonybolanyo@gmail.com>
2015-05-06 14:42:46 +0200

    Moved 'fortune cookie' functionality into module.

--- ablearning.js
+++ ablearning.js
@@ -1,15 +1,7 @@
 // Main app entry point
 
 var express = require('express');
-
-
-var fortunes = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
+var fortune = require('./lib/fortune.js');
 
 
 var app = express();
@@ -32,8 +24,7 @@ app.get('/', function(req, res) {
 
 // About
 app.get('/about', function(req, res) {
-	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-	res.render('about', { fortune: randomFortune });
+	res.render('about', { fortune: fortune.getFortune() });
 });
 
 // custom 404 page
